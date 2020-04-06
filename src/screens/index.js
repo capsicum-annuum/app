322488c11d2c6eb475582f5dba335327b712ef36
Ederson Machado <edersonmberti@gmail.com>
2020-04-06 20:22:15 -0300

    added base screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,1 +1,4 @@
-
+export { HomeScreen } from './home/home.screen'
+export { ProfileScreen } from './profile/profile.screen'
+export { LoginScreen } from './login/login.screen'
+export { RegisterScreen } from './register/register.screen'
