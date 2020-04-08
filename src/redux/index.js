5633bcc54d37800367c1e96ab60d7fa2a509dac3
Ederson Machado <edersonmberti@gmail.com>
2020-04-08 19:20:57 -0300

    update http base

---
+++ src/redux/index.js
@@ -0,0 +1,6 @@
+export { LOGIN_ACTIONS } from './login/login.constant'
+export { LoginActions } from './login/login.action'
+export { LoginReducer } from './login/login.reducer'
+export { LoginSaga } from './login/login.saga'
+
+export { makeRequest } from './http'
