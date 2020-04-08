66dc247b9a287b7ee7480436abbd3e5dd120c15c
Ederson Machado <edersonmberti@gmail.com>
2020-04-08 17:25:45 -0300

    added endpoints  and environments constants

---
+++ src/config/constants/endpoints.constants.js
@@ -0,0 +1,18 @@
+import { Environments } from 'app-config/constants'
+
+export class Endpoints {
+  static AUTHENTICATION = '/autenticacao'
+  static PROFILE = '/perfil'
+
+  static getApiUrl() {
+    const ENV = Environments.getCurrent()
+
+    return ENV.API_URL
+  }
+  
+  static getAuthorizationUrl() {
+    const ENV = Environments.getCurrent()
+
+    return ENV.API_AUTH_URL
+  }
+}
---
+++ src/config/constants/index.js
@@ -0,0 +1,2 @@
+export { Environments } from './environments.constants'
+export { Endpoints } from './endpoints.constants'
