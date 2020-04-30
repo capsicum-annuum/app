4d4ff0de7e23dc7c14790c9c2d07fda6105970af
Ederson Machado <edersonmberti@gmail.com>
2020-04-30 15:57:46 -0300

    update keyboard events on basescreen

--- src/components/base-screen/base-screen.component.js
+++ src/components/base-screen/base-screen.component.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView, StatusBar, Keyboard } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 
-export const BaseScreen = ({ children }) => {
+export const BaseScreen = ({
+  children,
+  keyboardDidHide,
+  keyboardDidShow,
+  keyboardWillHide,
+  keyboardWillShow,
+}) => {
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', keyboardDidShow)
+    Keyboard.addListener('keyboardDidHide', keyboardDidHide)
+    Keyboard.addListener('keyboardWillHide', keyboardWillHide)
+    Keyboard.addListener('keyboardWillShow', keyboardWillShow)
+
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', keyboardDidShow)
+      Keyboard.removeListener('keyboardDidHide', keyboardDidHide)
+      Keyboard.removeListener('keyboardWillHide', keyboardWillHide)
+      Keyboard.removeListener('keyboardWillShow', keyboardWillShow)
+    }
+  }, [])
+
   return (
     <LinearGradient style={{ flex: 1 }} colors={['#15CA9E', '#4AAECB']}>
-      <SafeAreaView />
-      {children}
+      <SafeAreaView style={{ flex: 1 }}>{children}</SafeAreaView>
     </LinearGradient>
   )
 }
