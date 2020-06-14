60f38d2653015311f3391bd8165b89d06ccc7fab
Ederson Machado <edersonmberti@gmail.com>
2020-06-14 02:37:53 -0300

    update pt-br file

--- src/config/locales/pt-br.js
+++ src/config/locales/pt-br.js
@@ -10,6 +10,8 @@ const ptBr = {
   },
   register: {
     write_about_you: 'Escreva sobre você',
+    voluntary: 'Voluntário',
+    organization: 'Organização',
     morning: 'manhã',
     afternoon: 'tarde',
     night: 'noite',
@@ -17,9 +19,54 @@ const ptBr = {
     morning_and_afternoon: 'manhã e tarde',
     morning_and_night: 'manhã e noite',
     afternoon_and_night: 'tarde e noite',
+    enable_location: 'Ativar Localização',
+    or_inform: 'Ou informe',
+    state: 'Estado',
+    city: 'Cidade',
+    neighborhood: 'Bairro',
+    access_gallery_denied:
+      'Desculpa, precisamos de acesso a galeria para isso funcionar!',
+    choose_profile: 'Escolha o perfil mais\nadequado para você',
+    which_profile: 'Não sabe qual perfil\nescolher?',
+    click_here: ' Click aqui',
+    has_account: 'Já possui conta?',
+    access_here: ' Acesse aqui',
+    voluntary_register: 'Cadastro de voluntário',
+    organization_register: 'Cadastro de organização',
+    full_name: 'Nome completo',
+    organization_name: 'Nome da organização',
+    email: 'E-mail',
+    cnpj: 'CPNJ',
+    phone: 'Telefone',
+    password: 'Senha',
+    password_confirmation: 'Confirmar senha',
+    register: 'Cadastrar',
+    continue: 'Continuar',
+    finish: 'Finalizar',
+    photo_title: 'Escolha a foto de perfil',
+    photo_description:
+      'Sua foto ficará visível para todos os usuários e organizações',
+    description_title: 'Fale um pouco sobre você',
+    description_description:
+      'Diga pontos importantes sobre você, assim as organizações podem te conhecer melhor',
+    skills_title: 'Selecione suas habilidades',
+    skills_description:
+      'Escolha os itens que te definem, isso aumenta as chances de você ser aceito em um evento',
+    causes_title: 'Quais causas você apoia',
+    causes_description:
+      'Isso nos ajudará a encontrar organizações que apoiam as mesmas causas que você',
+    availability_title: 'Defina sua disponibilidade',
+    availability_description:
+      'Selecione sua disponibilidade para encontrarmos os eventos mais adequados para você',
+    location_title: 'Informe sua localização',
+    location_description:
+      'Por último, informe seu endereço para que possamos encontrar ações perto de você',
   },
   ap_text_input: {
     optional: 'Opcional',
+  },
+  location_service: {
+    access_denied: 'O acesso à localização foi negado',
   },
 }
 
