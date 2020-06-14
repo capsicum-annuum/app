66fdbfcfdcfa96fd85b492b2688d735a7a8eb152
Ederson Machado <edersonmberti@gmail.com>
2020-06-14 02:27:13 -0300

    added register validators

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,2 +1,3 @@
 export { loginFormSchema } from './login-form-schema'
 export { registerFormSchema } from './register-form-schema'
+export { RegisterValidators } from './register-validators'
---
+++ src/validators/register-validators.js
@@ -0,0 +1,65 @@
+import { useSelector } from 'react-redux'
+import { strings } from 'app-locales'
+
+const photoValidator = () => {
+  return {
+    label: strings('register.continue'),
+    disabled: false,
+  }
+}
+
+const descriptionValidator = () => {
+  return {
+    label: strings('register.continue'),
+    disabled: false,
+  }
+}
+
+const causesValidator = () => {
+  const { causes } = useSelector((state) => state.RegisterReducer)
+
+  const disabled = causes.length === 0
+
+  return {
+    label: strings('register.continue'),
+    disabled,
+  }
+}
+
+const skillsValidator = () => {
+  const { skills } = useSelector((state) => state.RegisterReducer)
+
+  const disabled = skills.length === 0
+
+  return {
+    label: strings('register.continue'),
+    disabled,
+  }
+}
+
+const availabilityValidator = () => {
+  return {
+    label: strings('register.continue'),
+    disabled: false,
+  }
+}
+
+const locationValidator = () => {
+  const { location } = useSelector((state) => state.RegisterReducer)
+
+  const disabled = !location.state && !location.city
+
+  return {
+    label: strings('register.finish'),
+    disabled,
+  }
+}
+
+export const RegisterValidators = {
+  photoValidator,
+  descriptionValidator,
+  causesValidator,
+  skillsValidator,
+  availabilityValidator,
+  locationValidator,
+}
