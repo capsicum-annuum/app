6a1029fea4a63b0724295ada6a54dcd61a07d900
Ederson Machado <edersonmberti@gmail.com>
2020-06-28 11:52:15 -0300

    add base register finish screen

---
+++ src/screens/register/register-finish/register-finish.screen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const RegisterFinishScreen = () => {
+  return (
+    <View>
+      <Text>RegisterFinishScreen</Text>
+    </View>
+  )
+}
