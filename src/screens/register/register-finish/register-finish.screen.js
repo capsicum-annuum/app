7e7882d111b318cf1f47ab7552f502ac12156faf
Robson Rosa <edersonmachado@MacBook-Air-de-Ederson.local>
2020-07-04 14:58:12 -0300

    add register finish screen integration

--- src/screens/register/register-finish/register-finish.screen.js
+++ src/screens/register/register-finish/register-finish.screen.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, ActivityIndicator, Image } from 'react-native'
 import { BaseScreen } from 'app-components'
-import { strings } from 'app-locales'
 import { colors } from 'app-theme'
+import { Role, Screens } from 'app-constants'
+import { useSelector, useDispatch } from 'react-redux'
+import { strings } from 'app-locales'
+import { RegisterActions } from 'app-redux'
 
 import Styles from './register-finish.style'
 
 const rightImage = require('../../../assets/images/finish-register-right.png')
 const leftImage = require('../../../assets/images/finish-register-left.png')
 
-export const RegisterFinishScreen = () => {
-  const isVoluntary = false
+export const RegisterFinishScreen = ({ navigation }) => {
+  const {
+    role,
+    registerRequestSuccessData,
+    registerRequestErrorData,
+  } = useSelector((state) => state.RegisterReducer)
+  const isVoluntary = Role.VOLUNTARY === role
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (registerRequestSuccessData) {
+      navigation.navigate(Screens.MAIN_STACK)
+    }
+  }, [registerRequestSuccessData])
+
+  useEffect(() => {
+    if (registerRequestErrorData) {
+      navigation.goBack()
+    }
+  }, [registerRequestErrorData])
+
+  useEffect(() => {
+    dispatch(RegisterActions.registerRequest())
+  }, [])
 
   const text = isVoluntary
     ? strings('register.creating_volunteer_profile')
