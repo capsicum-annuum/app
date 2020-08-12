48a464f167f29908737e6dcb2170af14a19b59c9
Ederson Machado <edersonmberti@gmail.com>
2020-08-12 14:31:49 -0300

    add location reducer

--- src/redux/app.saga.js
+++ src/redux/app.saga.js
@@ -2,9 +2,10 @@ import { all, fork } from 'redux-saga/effects'
 
 import { LoginSaga } from './login/login.saga'
 import { RegisterSaga } from './register/register.saga'
+import { LocationSaga } from './location/location.saga'
 
 function* AppSagas() {
-  yield all([fork(LoginSaga), fork(RegisterSaga)])
+  yield all([fork(LoginSaga), fork(RegisterSaga), fork(LocationSaga)])
 }
 
 export { AppSagas }
